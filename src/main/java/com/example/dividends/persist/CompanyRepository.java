c29e88824ac3cb36c5a7e7836bb01768924fb8a3
UNA <lemonlee030@gmail.com>
2023-12-11 21:32:43 +0900

    create

---
+++ src/main/java/com/example/dividends/persist/CompanyRepository.java
@@ -0,0 +1,15 @@
+package com.example.dividends.persist;
+
+import com.example.dividends.persist.entity.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CompanyRepository  extends JpaRepository<CompanyEntity, Long> {
+    boolean existsByTicker(String ticker);
+
+    Optional<CompanyEntity> findByName(String name);
+
+}
---
+++ src/main/java/com/example/dividends/persist/entity/DividendEntity.java
@@ -0,0 +1,35 @@
+package com.example.dividends.persist.entity;
+
+import com.example.dividends.model.Dividend;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "DIVIDEND")
+@Getter
+@ToString
+@NoArgsConstructor
+public class DividendEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long companyId;
+
+    private LocalDateTime date;
+
+    private String dividend;
+
+    public DividendEntity(Long companyId, Dividend dividend){
+        this.companyId = companyId;
+        this.date = dividend.getDate();
+        this.dividend = dividend.getDividend();
+    }
+}
